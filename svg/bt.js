ed3f923dd71026b677db41b7c30ef546c6c5ad53
janocampo <janocampo2015@gmail.com>
2017-06-29 17:44:51 -0700

    Implemented the User Control and Pointers

--- svg/bt.js
+++ svg/bt.js
@@ -1,6 +1,6 @@
 // Global variables
-var paper; // Used for Raphael
-
+var paper, v_ptr, p_ptr, r_ptr; // Used for Raphael
+// POinters as globals 
 
 /* This function creates the nodes on the image. Acts like a struct.*/
 function createNode(id, x, y) {
@@ -10,6 +10,8 @@ function createNode(id, x, y) {
 	this.id_text = paper.text(x, y, id).attr({"font-size": 14, "font-family" : "Times New Roman"});
 }
 
+
+// Draws the actual tree into the document
 function drawTree() {
 	// First Level - root
 	var node_five = createNode(5, 250, 50);
@@ -21,91 +23,139 @@ function drawTree() {
 	// Second Level
 	var node_three = createNode(3, 200, 100);
 
-	// Connectors for three
-	paper.path("M183, 110 ,L155,150");
-	paper.path("M200, 100 ,L250,150");
-	paper.path("M300, 100 ,L345,150");
+	// Connectors for three and seven
+	paper.path("M183, 110 ,L150,130"); // 3 - 2
+	paper.path("M218, 110 ,L250,130"); // 3 - 4
+	paper.path("M318, 110 ,L345,130"); // 7 - 8
 
 	var node_seven = createNode(7, 300, 100);
 
 	// Third level - left side
-	var node_two = createNode(2, 155, 150);
+	var node_two = createNode(2, 150, 150);
 
-	paper.path("M155, 150 ,L150,200");
+	paper.path("M150, 170 ,L150,180"); // 2 - 1
 
 	var node_four = createNode(4, 250, 150);
 	// var node_six = createNode(6, 275, 150);
+
 	// Third level - right side
-	var node_eight = createNode(8, 345, 150);
+	var node_eight = createNode(8, 350, 150);
+
 	// fourth level - left side 
 	var node_one = createNode(1, 150, 200);
-}
 
-function drawV(x, y) {
-        this.circle = paper.circle(x, y, 23);
-        this.circle.attr("stroke", "green");
-        this.circle.attr("stroke-width", 2)
-}
+	// fifth level - NULL NODES
+	var null_node_one = createNode("NULL", 150, 250);
+	var null_node_four = createNode("NULL", 250, 250);
+	var null_node_eight = createNode("NULL", 350, 250);
+	var null_node_seven = createNode("NULL", 300, 250);
 
-function drawP(x, y) {
-        this.circle = paper.circle(x, y, 26);
-        this.circle.attr("stroke", "blue");
-        this.circle.attr("stroke-width", 2)
-}
-
-function drawR(x, y) {
-        this.circle = paper.circle(x, y, 29);
-        this.circle.attr("stroke", "red");
-        this.circle.attr("stroke-width", 2)
-}
-
-function removeV(x, y) {
-        this.circle = paper.circle(x, y, 23);
-        this.circle.attr("stroke", "white");
-        this.circle.attr("stroke-width", 2.3)
-}
-
-function removeP(x, y) {
-        this.circle = paper.circle(x, y, 26);
-        this.circle.attr("stroke", "white");
-        this.circle.attr("stroke-width", 2.3)
-}
-
-function removeR(x, y) {
-        this.circle = paper.circle(x, y, 29);
-        this.circle.attr("stroke", "white");
-        this.circle.attr("stroke-width", 2.3)
+	// add lines to null Nodes
+	paper.path("M150, 220, L150, 230"); // 1 - NULL
+	paper.path("M250, 170, L250, 230"); // 4 - NULL
+	paper.path("M300, 120, L300, 230"); // 7 - NULL
+	paper.path("M350, 170, L350, 230"); // 8 - NULL
+	paper.path("M170, 150, L250, 230"); // 2 - NULL
 }
 
 
-// TODO: Draw Key Guide
+
+//  Draw Key Guide with colors 
 function drawKey() {
-	var guide = paper.text(50, 50, "Pointer Key").attr({'font-size': 14, "font-family": "Times New Roman"});
+	paper.text(50, 50, "Pointer Key").attr({'font-size': 14, "font-family": "Times New Roman"});
 	var v = paper.circle(25, 100, 10);
-	var v_text = paper.text(65, 100, "= V");
+	paper.text(65, 100, "= V");
 	var parent = paper.circle(25, 150, 10);
-	var parent_text = paper.text(65, 150, "= Parent");
+	paper.text(65, 150, "= Parent");
 	var root_ptr = paper.circle(25, 200, 10);
-	var root_text = paper.text(65, 200, "= root");
+	paper.text(65, 200, "= root");
 
 	v.attr("fill", "green");
 	parent.attr("fill", "blue");
 	root_ptr.attr("fill", "red");
-
 }
 
-// TODO: Functionality
-
-
 window.onload = function() {
-	paper = new Raphael(document.getElementById('tree'), 500, 400);
+	paper = new Raphael(document.getElementById('tree'), 500, 300);
 	drawTree();
 	drawKey();
 
-	var vPtr = drawV(250, 50);
-    var pPtr = drawP(250, 50);
-    var rPtr = drawR(250, 50);
-    removeV(250, 50);
-    removeP(250, 50);
-    removeR(250, 50);
+	// Used to keep track of the pointer coordinates
+	var current_x = 250, current_y = 50;
+
+	// Setting up the pointers
+	v_ptr = paper.circle(250, 50, 23);
+	v_ptr.attr("stroke", "green");
+	v_ptr.attr("stroke-width", 2);
+
+	r_ptr = paper.circle(250, 50, 27);
+	r_ptr.attr("stroke", "red");
+	r_ptr.attr("stroke-width", 2);
+
+	p_ptr = paper.text(250, 5, "NULL");
+	p_ptr.attr({"fill" : "blue", "font-size" : 14, "font-family": "Times New Roman"});
+
+	$("#submitBtn").click ( function() {
+        if ( $('select#executeQuery').val() === "pToV") {
+        	p_ptr.remove();
+        	p_ptr = paper.circle(current_x, current_y, 25);
+        	p_ptr.attr("stroke", "blue");
+        	p_ptr.attr("stroke-width", 2);
+        	if (current_y == 250) {
+        		alert("Parent has reached Null");
+        	}
+        }
+        else if ( $('select#executeQuery').val() === "vLeft") {
+        	// Account for user query to go down further after NULL has already been reaced
+        	if (current_y == 250) {
+        		alert("You cannot proceed further down as the pointer has reached NULL");
+        		// Refresh page here
+        		history.go(0);
+
+        	}
+        	// Account for NULL Cases
+        	else if ((current_x == 350 && current_y == 150) || (current_x == 300 && current_y == 100) || (current_x == 250 && current_y == 150) || (current_x == 150 & current_y == 200)) {
+        		v_ptr.remove();
+        		current_y = 250;
+        		v_ptr = paper.circle(current_x, current_y, 23).attr({"stroke" : "green", "stroke-width" : 2});
+        		alert("V Pointer has reached Null");
+        	}
+        	else if (current_x == 150 && current_y == 150) {
+        		v_ptr.remove();
+        		current_y = current_y + 50;
+        		v_ptr = paper.circle(current_x, current_y, 23).attr({"stroke": "green", "stroke-width": 2});
+        	}
+        	// Normal Case
+        	else {
+        		v_ptr.remove();
+        		current_x = current_x - 50;
+        		current_y = current_y + 50;
+        		v_ptr = paper.circle(current_x, current_y, 23).attr({"stroke": "green", "stroke-width": 2});
+        	}
+
+        }
+        else if ( $('select#executeQuery').val() === "vRight") {
+        	// CASE !: IF ALREADY ON NULL -> MSG
+        	if (current_y == 250) {
+        		alert("You cannot proceed further down as the pointer has reached NULL");
+        		history.go(0);
+        	}
+        	// CASE 2: IF V WILL LEAD TO NULL -> MSG
+        	else if ((current_x == 150) || (current_x == 250 && current_y == 150) || (current_x == 350 && current_y == 150)) {
+        		v_ptr.remove();
+        		current_y = 250;
+        		v_ptr = paper.circle(current_x, current_y, 23).attr({"stroke" : "green", "stroke-width" : 2});
+        		alert("V pointer has reached NULL");
+        	}
+        	// CASE 4: NORMAL 
+        	else {
+        		v_ptr.remove();
+        		current_x = current_x + 50;
+        		current_y = current_y + 50;
+        		v_ptr = paper.circle(current_x, current_y, 23).attr({"stroke": "green", "stroke-width": 2});
+
+        	}
+        }
+        else alert("Something broke!");
+      }) // END SUBMIT BUTTON 
 }
